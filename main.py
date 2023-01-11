4ddfda28a5cf4f4935d9115b9f6eead2cdb4fb4d
vitor f lima <vitor@vitor.local>
2023-01-10 21:42:00 -0300

    fiste commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, Response
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    
